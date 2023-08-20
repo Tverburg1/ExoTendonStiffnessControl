d46212baa6edc7f1bffdb6a93f95152deb8e9d61
Tim Verburg <tim.verburg@hotmail.com>
2023-08-20 14:27:28 +0200

    Changed the code to be compatible with the matlab script

--- ExoTendonStiffnessControl.ino
+++ ExoTendonStiffnessControl.ino
@@ -2,22 +2,20 @@ signed int des_pos;
 float des_stiffness;
 #include "loadcell_read.h"
 #include "motor_control.h"
-#include "stiffness_control.h"
 
-
-#include <Servo.h>
+//#include <Servo.h>
 
 // Servo --------------------------------------
 int servo_pin = 10;
-Servo stiffservo;
+//Servo stiffservo;
 bool startStiffTest;
 int servoPos = 4095;
 int servoLimit = 4095;
-int stage = 0;
+int stage = 1;
 int counter = 0;
 int counter_threshold = 100;
-int millis_threshold = 4000;
-int start_delay = 4000;
+int millis_threshold = 2000;
+int start_delay = 2000;
 int angleStep = 2048;
 int t0 = 0;
 int t1 = 0;
@@ -25,66 +23,52 @@ int t2 = 0;
 bool start = false;
 bool reverse = false;
 bool restart = false;
-int testStiffnesses[] = {400, 375, 350, 325, 300, 275, 250, 225, 200, 175, 150};
+int resetStiffness = 100000;
+
 
 
 String  incomingSerialData;   // for incoming serial data
 void setup() {
 
   Serial.begin(115200);
-//  Serial.setTimeout(10);
-  initialize_loadcell();
+  Serial.setTimeout(10);
+//  initialize_loadcell();
   initialize_motor();
   setup_rotary_encoder();
   unlock_motor();
 
   configure_motor();
-
-  stiffservo.attach(servo_pin);
-  stiffservo.write(servoPos);
+//
+//  stiffservo.attach(servo_pin);
+//  stiffservo.write(servoPos);
 }
 
 void loop() {
   if (Serial.available() > 0)
   {
     incomingSerialData = Serial.readString();
-    if (incomingSerialData == "S\n"){
+    if (incomingSerialData.equals("S\n")){
       startStiffTest = true;
     }
+    else{
+      // Split the received string into two integer values using parseInt
+      des_stiffness = incomingSerialData.toInt();
+      
+//      int spaceIndex = incomingSerialData.indexOf(' ');
+//      
+//      if (spaceIndex != -1) {
+//        des_stiffness = incomingSerialData.substring(0, spaceIndex).toInt();
+//        servoPos = incomingSerialData.substring(spaceIndex + 1).toInt();
+//      }
+    }
   }
-//  read_position();
+
+  
   stiffnessMotorControl(des_stiffness);
+  
   Serial.print(", Loadcell reading: ");Serial.print(calc_loadcell_output());
   Serial.print(", Servo position: ");Serial.println(servoPos);
 //  printData();
   delay(100);
-  //Serial.print(calc_loadcell_output());
 
-  if (startStiffTest) {
-    if (t0 == 0){
-      t0 = millis();
-    }
-    
-    if (true){
-      if( stage == 11){
-        des_stiffness = 0;
-        servoPos = 4095;
-      }
-      else{
-        des_stiffness = testStiffnesses[stage];
-        servoPos = 4095 - servoPosConstantForce(400, testStiffnesses[stage], 0.03);
-        t1 = millis();
-  
-        if (t1-t0 >= start_delay && not start){
-          start = true;
-          t0 = millis();
-        }
-        
-        if (t1-t0 >= millis_threshold && start){
-            start = false;
-            stage ++;
-        }
-      }
-    }    
-  } 
 }
